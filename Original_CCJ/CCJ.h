5f1b936a738ce9bbe7866c2febcac50c0d980051
IanWark <wark@ualberta.ca>
2018-03-26 15:54:22 -0600

    get rid of Original CCJ

--- Original_CCJ/CCJ.h
+++
@@ -1,6 +0,0 @@
-#ifndef HFOLD_H_
-#define HFOLD_H_
-
-double ccj(char *sequence, char *structure);
-
-#endif /*HFOLD_H_*/
--- Sparse_CCJ/CCJ.h
+++
@@ -1,8 +0,0 @@
-#ifndef HFOLD_H_
-#define HFOLD_H_
-
-#include "h_struct.h"
-
-double ccj(char *sequence, char *structure);
-
-#endif /*HFOLD_H_*/
